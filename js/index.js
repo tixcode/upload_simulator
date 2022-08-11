b9b0c532c66ddd0ea42815b7b21c8621bca00820
Emnoze <emnoze@gmail.com>
2022-08-11 15:59:50 +0200

    Adding selecting language box + starting under construction french pages

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+
+    $("select" ).change(function() {
+
+        const langageSelected = $( "select option:selected" ).val();
+        const wikiLink = $("#wikiLink");
+        const changelogsLink = $("#changelogsLink");
+
+        wikiLink.attr(
+            'href',
+            wikiLink.attr('href')
+                .slice(0, -3) + langageSelected + "/"
+        );
+
+        changelogsLink.attr(
+            'href',
+            changelogsLink.attr('href')
+                .slice(0, -3) + langageSelected + "/"
+        );
+
+        let htmlTextWiki = wikiLink.html()
+
+        let replacementWikiText = '';
+        let replacementChangelogText = '';
+        switch (langageSelected) {
+            case 'en':
+                replacementWikiText = 'Wikipedia'
+                replacementChangelogText = 'Changelogs'
+                break;
+            case 'ru':
+                replacementWikiText = 'Википедия'
+                replacementChangelogText = 'Список Изменений'
+                break;
+            case 'fr':
+                replacementWikiText = 'Wiki'
+                replacementChangelogText = 'Changements'
+                break;
+            default:
+                console.log(`Sorry, we are out of ${expr}.`);
+        }
+        wikiLink.html(htmlTextWiki.replace(/>[a-zA-Z]+</, ">"+replacementWikiText+"<"))
+        changelogsLink.html(htmlTextWiki.replace(/>[a-zA-Z]+</, ">"+replacementChangelogText+"<"))
+
+
+    });
+
+});
+
+
